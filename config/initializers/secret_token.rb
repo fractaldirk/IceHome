039ee003ec8e40de34ef999b117059474d12f517
fractaldirk <dirkgrandjean@live.nl>
2013-05-16 14:23:18 +0200

    first homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IceHome::Application.config.secret_token = 'cf050ba8390bf496067abdfffed603fea8f3fd579e26a72359f37b15fe6dc5c6b7cd8d07b932195df8a3b6da795eed084d102be6d68b2b9b991411ef15ba3240'
